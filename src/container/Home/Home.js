bc1132b14d2308dc27e90fdaa5ba0c302d7b6555
Melissa Astbury <melissajastbury@gmail.com>
2020-07-21 14:41:02 +0200

    completed the home page by adding in 3 section with text and an image. Text is dummy text. Styles have also been added

--- src/container/Home/Home.js
+++ src/container/Home/Home.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import "./Home.scss";
 import mainCoffeeImage from "../../assets/coffee-image.jpg";
+import coffeeBeanImage from "../../assets/coffee-beans.jpeg";
+import foodImage from "../../assets/food-image.jpeg";
 
 import LocationMap from "../../components/Map/Map";
 
@@ -16,7 +18,58 @@ const Home = () => {
         />
         <h1 className="sub-title">Espresso yourself!</h1>
       </div>
-      <LocationMap />
+      <div className="description-container">
+        <div className="description">
+          <h3 className="description-title">
+            Start your day off great with a cup of coffee!
+          </h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+            lacinia, lorem eu dignissim cursus, massa lacus tincidunt dui, a
+            sagittis dolor urna quis quam. Suspendisse pharetra ligula vitae
+            tellus tempus gravida. Curabitur sollicitudin eget metus at aliquam.
+            Sed iaculis accumsan erat a ornare. Ut pellentesque posuere dui sit
+            amet consequat. Morbi sed justo est.
+          </p>
+        </div>
+        <LocationMap className="map" />
+      </div>
+      <div className="description-container">
+        <img
+          src={coffeeBeanImage}
+          alt="Coffee cup with spilled coffee beans"
+          className="coffee-image"
+        />
+        <div className="description">
+          <h3 className="description-title">The best coffee around</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+            lacinia, lorem eu dignissim cursus, massa lacus tincidunt dui, a
+            sagittis dolor urna quis quam. Suspendisse pharetra ligula vitae
+            tellus tempus gravida. Curabitur sollicitudin eget metus at aliquam.
+            Sed iaculis accumsan erat a ornare. Ut pellentesque posuere dui sit
+            amet consequat. Morbi sed justo est.
+          </p>
+        </div>
+      </div>
+      <div className="description-container">
+        <div className="description">
+          <h3 className="description-title">Take a look at our menu!</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+            lacinia, lorem eu dignissim cursus, massa lacus tincidunt dui, a
+            sagittis dolor urna quis quam. Suspendisse pharetra ligula vitae
+            tellus tempus gravida. Curabitur sollicitudin eget metus at aliquam.
+            Sed iaculis accumsan erat a ornare. Ut pellentesque posuere dui sit
+            amet consequat. Morbi sed justo est.
+          </p>
+        </div>
+        <img
+          src={foodImage}
+          alt="Sliced Hamon Serrano on black slate"
+          className="food-image"
+        />
+      </div>
     </div>
   );
 };
